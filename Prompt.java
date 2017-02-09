744dd08423533bb566994cd2cc42d073b30dd57f
dmc0204 <dmc0204@gmail.com>
2017-02-09 12:05:18 -0500

    Add files via upload

---
+++ Prompt.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dejanuz;
+
+/**
+ *
+ * @author dcummins
+ */
+public class Prompt {
+    
+        public void test(){
+    
+    System.out.println("Prompt:Pass");
+    
+}
+    
+}
